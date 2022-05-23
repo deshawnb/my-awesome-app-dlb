0efe72d987cba4abc81c8b71d2ee2950e677d71c
Deshawn Brown <brownd726@gmail.com>
2022-05-23 11:14:21 -0500

    feat: Created a simple Node 'hello World' app.

---
+++ App.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+// simple hello word node app
+const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
+    res.send('hello world!')
+}); app.listen(APP_PORT, () => {
+    console.log(`running app at port:${APP_PORT}`)
+})
