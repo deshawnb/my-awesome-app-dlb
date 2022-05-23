49d8ad061b5b5ada4f37d08aceaa330ef8bcf4ab
Deshawn Brown <brownd726@gmail.com>
2022-05-23 13:17:07 -0500

    style: changed message in app.js

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 // simple hello word node app
 const APP_PORT = process.env.APP_PORT || 3000; app.get('/', (req, res) => {
-    res.send('hello world!')
+    res.send('hello deshawn! Nice to see you!')
 }); app.listen(APP_PORT, () => {
     console.log(`running app at port:${APP_PORT}`)
 })
